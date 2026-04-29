20eb65f212a0d94387b5a6d858ff71350f207d23
lawrencedovin <lawrencedovin@gmail.com>
2026-04-29 13:38:56 -0400

    Updated for Template Essentials section

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="app">
+    <h1 (click)="handleClick($event)" #heading>
+      {{ newMessage ? newMessage : message }}
+    </h1>  
+    <input [value]="message" (input)="newMessage = messageInput.value" #messageInput>
+    <p>{{ messageInput.value }}</p>
+  </div>`,
+  styles: [
+    `
+      .app {
+        margin-top: 50px;
+        font-size: 22px;
+        color: #fff;
+        text-align: center;
+      }
+    `,
+  ],
+})
+export class AppComponent implements OnInit {
+  message!: string;
+  newMessage!: string;
+
+  ngOnInit() {
+    this.message = 'Hello World';
+  }
+
+  handleClick(event: Event) {
+    console.log(event);
+  }
+}

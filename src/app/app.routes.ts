1339b1c1c3e0662b6b6fd885f6a882f158b4b251
lawrencedovin <lawrencedovin@gmail.com>
2026-05-18 14:13:42 -0400

    Updated AppModule to be app routes file for standalone component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { Routes } from '@angular/router';
+import { DonutService } from './admin/services/donut.service';
+
+export const AppRoutes: Routes = [
+  { 
+    path: 'admin', 
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    providers: [importProvidersFrom(HttpClientModule), DonutService]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'admin'
+  },
+  { path: '**', redirectTo: 'admin'}
+];
